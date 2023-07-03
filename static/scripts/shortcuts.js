8d87989da5d0009b14ee03169c579b3677aa719a
DevDevil <captainaydin11@gmail.com>
2023-07-02 21:48:09 -0400

    FIxing shortcuts

--- static/scripts/shortcuts.js
+++ static/scripts/shortcuts.js
@@ -1,9 +1,9 @@
 document.addEventListener('keydown', function (event) {
     if (event.altKey && event.key === 'c') {
       var websites = [
-        '/routes/cloaks/howtobelogical.html',
-        '/routes/cloaks/mathisfun.html',
-        '/routes/cloaks/vocabulary.html'
+        '/table',
+        '/math',
+        '/vocab'
       ];
       var randomIndex = Math.floor(Math.random() * websites.length);
       var randomWebsite = websites[randomIndex];
