dc3ac1a4cacc3c16ee9219022da5ccd18b2b5af4
DevDevil <captainaydin11@gmail.com>
2023-07-05 14:16:26 -0400

    Adding more shortcut links

--- static/scripts/shortcuts.js
+++ static/scripts/shortcuts.js
@@ -23,8 +23,28 @@ document.addEventListener('keydown', function (event) {
     'https://www.mathisfun.com',
     'https://www.aaamath.com',
     'https://www.nctm.org',
-    'https://www.mathgoodies.com'
-  ];
+    'https://www.mathgoodies.com',
+    'https://www.mathsisfun.com',
+    'https://www.education.com/math',
+    'https://www.math-drills.com',
+    'https://www.multiplication.com',
+    'https://www.mathantics.com',
+    'https://www.hoodamath.com',
+    'https://www.math-aids.com',
+    'https://www.ixl.com/precalculus',
+    'https://www.mathplanet.com',
+    'https://www.cut-the-knot.org',
+    'https://www.mathwarehouse.com',
+    'https://www.ixl.com/geometry',
+    'https://www.cymath.com',
+    'https://www.mathsisawesome.com',
+    'https://www.ixl.com/algebra',
+    'https://www.themathpage.com',
+    'https://www.mathway.org',
+    'https://www.ixl.com/calculus',
+    'https://www.mangahigh.com',
+    'https://www.mathnook.com'
+];
   
   var scienceWebsites = [
     'https://www.nasa.gov',
@@ -36,8 +56,28 @@ document.addEventListener('keydown', function (event) {
     'https://www.smithsonianmag.com/science-nature',
     'https://www.livescience.com',
     'https://www.discovery.com',
-    'https://www.sciencemag.org'
-  ];
+    'https://www.sciencemag.org',
+    'https://www.sciencedaily.com',
+    'https://www.nature.com',
+    'https://www.newscientist.com',
+    'https://www.pbs.org/wgbh/nova',
+    'https://www.space.com',
+    'https://www.sciencemuseum.org.uk',
+    'https://www.sciencekids.co.nz',
+    'https://www.exploratorium.edu',
+    'https://www.iflscience.com',
+    'https://www.sciencenordic.com',
+    'https://www.cnet.com/news/science',
+    'https://www.sciencemagforstudents.org',
+    'https://www.howstuffworks.com/science',
+    'https://www.aps.org',
+    'https://www.sciencebuddies.org',
+    'https://www.sciencealert.com',
+    'https://www.sciencenotebooking.blogspot.com',
+    'https://www.scienceworld.ca',
+    'https://www.sciencebob.com',
+    'https://www.sciencewithme.com'
+];
   
   var englishWebsites = [
     'https://www.dictionary.com',
@@ -49,8 +89,28 @@ document.addEventListener('keydown', function (event) {
     'https://www.eslgold.com',
     'https://www.learnenglish.de',
     'https://www.macmillandictionary.com',
-    'https://www.engvid.com'
-  ];
+    'https://www.engvid.com',
+    'https://www.oxfordlearnersdictionaries.com',
+    'https://www.cambridge.org/elt',
+    'https://www.englishpage.com',
+    'https://www.englishcentral.com',
+    'https://www.duolingo.com',
+    'https://www.busuu.com',
+    'https://www.englishforums.com',
+    'https://www.english-at-home.com',
+    'https://www.englishgrammar.org',
+    'https://www.ef.com',
+    'https://www.englishtown.com',
+    'https://www.fluentu.com',
+    'https://www.memrise.com',
+    'https://www.elllo.org',
+    'https://www.talkenglish.com',
+    'https://www.engfluent.com',
+    'https://www.myenglishpages.com',
+    'https://www.englishleap.com',
+    'https://www.espressoenglish.net',
+    'https://www.englishspeak.com'
+];
   
   window.addEventListener("load", () => {
     navigator.serviceWorker.register("../sw.js", {
