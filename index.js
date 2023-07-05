760d92ac076914472867e0825e48f0086e8c9d14
DevDevil <captainaydin11@gmail.com>
2023-07-05 19:25:52 -0400

    updating i guess

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import http from "node:http";
-import createBareServer from "@tomphttp/bare-server-node";
+import { createBareServer } from "@tomphttp/bare-server-node";
 import path from "node:path";
 import * as dotenv from "dotenv";
 dotenv.config();
@@ -11,7 +11,7 @@ const app = express(server);
 const bareServer = createBareServer("/geometry/");
 
 app.use(express.json());
-app.use(
+app.use(  
   express.urlencoded({
     extended: true,
   })
