6d64df8f4593f87ddde6fe7f8c704627d97e9edd
DevDevil <captainaydin11@gmail.com>
2023-07-22 13:20:28 -0400

    Before background change

--- static/scripts/backgroundimage.js
+++ static/scripts/backgroundimage.js
@@ -1,31 +1,40 @@
-const imageUrlInput = document.getElementById("imageUrl");
-let canvasElement = document.getElementById("canvas");
+document.addEventListener("DOMContentLoaded", function() {
+  const imageUrlInput = document.getElementById("imageUrl");
+  let canvasElement = document.getElementById("canvas");
 
-function changeBackground() {
-  const imageUrl = imageUrlInput.value;
-  if (!imageUrl) {
-    alert("Please enter a valid image URL.");
-    return;
+  function changeBackground() {
+    const imageUrl = imageUrlInput.value;
+    if (!imageUrl) {
+      alert("Please enter a valid image URL.");
+      return;
+    }
+
+    // Remove the canvas from the DOM
+    if (canvasElement) {
+      canvasElement.parentNode.removeChild(canvasElement);
+      canvasElement = null;
+    }
+
+    document.body.style.backgroundColor = "transparent";
+    document.body.style.backgroundImage = `url('${imageUrl}')`;
   }
 
-  // Remove the canvas from the DOM
-  if (canvasElement) {
-    canvasElement.parentNode.removeChild(canvasElement);
-    canvasElement = null;
+  function resetBackground() {
+    // Check if the canvas element exists, and if not, create it and add it back to the DOM
+    if (!canvasElement) {
+      canvasElement = document.createElement("div");
+      canvasElement.id = "canvas";
+      document.body.appendChild(canvasElement);
+    }
+
+    document.body.style.backgroundColor = "#e0e0e0";
+    document.body.style.backgroundImage = "none";
   }
 
-  document.body.style.backgroundColor = "transparent";
-  document.body.style.backgroundImage = `url('${imageUrl}')`;
-}
+  // Attach click event listeners to buttons
+  const submitBtn = document.getElementById("submitBtn");
+  const resetBtn = document.getElementById("resetBtn");
 
-function resetBackground() {
-  // Check if the canvas element exists, and if not, create it and add it back to the DOM
-  if (!canvasElement) {
-    canvasElement = document.createElement("div");
-    canvasElement.id = "canvas";
-    document.body.appendChild(canvasElement);
-  }
-
-  document.body.style.backgroundColor = "#e0e0e0";
-  document.body.style.backgroundImage = "none";
-}
+  submitBtn.addEventListener("click", changeBackground);
+  resetBtn.addEventListener("click", resetBackground);
+});
